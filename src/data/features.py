7b8b6d0529499a40ce5f970516a60e108148d2fe
thanhtvt <19021367@vnu.edu.vn>
2023-06-20 21:19:07 -0600

    [EDIT] update eigenspectrum

--- src/data/features.py
+++ src/data/features.py
@@ -39,54 +39,16 @@ def represent(
     return X_after
 
 
-class Input:
-
-    def __init__(self, X):
-        self.step = 1.0
-        self.noise = 1.0
-        self.triuind = (np.arange(23)[:, np.newaxis]
-                        <= np.arange(23)[np.newaxis, :]).flatten()
-        self.max = 0
-        for _ in range(10):
-            self.max = np.maximum(self.max, self.realize(X).max(axis=0))
-        X = self.expand(self.realize(X))
-        self.nbout = X.shape[1]
-        self.mean = X.mean(axis=0)
-        self.std = (X - self.mean).std()
-
-    def realize(self, X):
-
-        def _realize_(x):
-            inds = np.argsort(
-                -(x**2).sum(axis=0)**.5 +
-                np.random.normal(0, self.noise, x[0].shape))
-            x = x[inds, :][:, inds] * 1
-            x = x.flatten()[self.triuind]
-            return x
-
-        return np.array([_realize_(z) for z in X])
-
-    def expand(self, X):
-        Xexp = []
-        for i in range(X.shape[1]):
-            for k in np.arange(0, int(self.max[i]) + int(self.step), int(self.step)):
-                Xexp += [np.tanh((X[:, i] - k) / self.step)]
-        return np.array(Xexp).T
-
-    def normalize(self, X):
-        return (X - self.mean) / self.std
-
-    def forward(self, X):
-        X = self.realize(X)
-        X = self.expand(X)
-        X = self.normalize(X)
-        return X.astype('float32')
-
-
-def get_eigenspectrum(coulomb: np.ndarray) -> np.ndarray:
-    values = np.linalg.eigvalsh(coulomb)
-    # values = np.sort(values)[::-1]    # cause bug
-    return values
+def get_eigenspectrum(coulomb: np.ndarray, sort: bool = False) -> np.ndarray:
+    eigs = np.linalg.eigvalsh(coulomb)
+    if sort:
+        inds = np.argsort(np.abs(eigs))[::-1]
+        n_samples = coulomb.shape[0]
+        if eigs.ndim == 2:
+            eigs = eigs[np.arange(n_samples)[:, None], inds]
+        elif eigs.ndim == 1:
+            eigs = eigs[inds]
+    return eigs
 
 
 def sort_coulomb(coulomb: np.ndarray) -> np.ndarray:
